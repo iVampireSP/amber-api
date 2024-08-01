50b2b03fdbc6c52707b3b343335c29b52508955e
Twilight <im@ivampiresp.com>
2024-08-02 03:16:55 +0800

    修复 缓存到期时间 TODO 添加 LLM 响应

--- internal/api/v1/chat.go
+++ internal/api/v1/chat.go
@@ -12,6 +12,7 @@ import (
 	"rag-new/internal/schema"
 	"rag-new/internal/service/auth"
 	"rag-new/internal/service/chat"
+	"rag-new/internal/service/llm"
 	"rag-new/pkg/consts"
 	"rag-new/pkg/random"
 	"strconv"
@@ -21,10 +22,11 @@ type ChatController struct {
 	authService *auth.Service
 	chatService *chat.Service
 	redis       *redis.Client
+	llmService  *llm.Service
 }
 
-func NewChatController(authService *auth.Service, chatService *chat.Service, redis *redis.Client) *ChatController {
-	return &ChatController{authService, chatService, redis}
+func NewChatController(authService *auth.Service, chatService *chat.Service, redis *redis.Client, llmService *llm.Service) *ChatController {
+	return &ChatController{authService, chatService, redis, llmService}
 }
 
 // List godoc
@@ -270,10 +272,101 @@ func (u *ChatController) generateChatStream(c context.Context, chatId string) (s
 		return "", err
 	}
 
-	err = u.redis.Set(c, u.getCacheKey("stream:"+randomId), chatId, 0).Err()
+	err = u.redis.Set(c, u.getCacheKey("stream:"+randomId), chatId, consts.ChatStreamExpire).Err()
 	if err != nil {
 		return "", err
 	}
 
 	return randomId, nil
+}
+
+// Stream godoc
+// @Summary      流式传输聊天内容
+// @Description  get string by ID
+// @Tags         chat_message
+// @Accept       json
+// @Produce      json
+// @Param        id  path  int  true  "Chat ID"
+// @Param        stream_id  path  string  true  "Chat stream id"
+// @Success      200  {object}  schema.ResponseBody{data=schema.ChatMessageResponse}
+// @Failure      400  {object}  schema.ResponseBody
+// @Failure      404  {object}  schema.ResponseBody
+// @Failure      409  {object}  schema.ResponseBody{data=schema.ChatMessageResponse}
+// @Failure      500  {object}  schema.ResponseBody
+// @Router       /api/v1/stream/{stream_id} [get]
+func (u *ChatController) Stream(c *gin.Context) {
+	var response = schema.NewResponse(c)
+	// 检查 stream id 是否存在
+	streamIdStr := c.Param("stream_id")
+	streamIdCacheKey := u.getCacheKey("stream:" + streamIdStr)
+
+	// 检查缓存是否存在
+	i, err := u.redis.Exists(c, streamIdCacheKey).Result()
+	if err != nil {
+		response.Status(http.StatusInternalServerError).Error(err).Send()
+		return
+	}
+
+	if i == 0 {
+		response.Status(http.StatusNotFound).Error(consts.ErrChatStreamNotFound).Send()
+		return
+	}
+
+	// 获取 chat id
+	chatIdStr, err := u.redis.Get(c, streamIdCacheKey).Result()
+	if err != nil {
+		response.Status(http.StatusInternalServerError).Error(err).Send()
+		return
+	}
+
+	chatId, err := strconv.Atoi(chatIdStr)
+	if err != nil {
+		response.Status(http.StatusInternalServerError).Error(err).Send()
+		return
+	}
+
+	chatEntity, err := u.chatService.GetChat(c, int64(chatId))
+	if err != nil {
+		response.Status(http.StatusInternalServerError).Error(err).Send()
+		return
+	}
+
+	if chatEntity.ID == consts.NoRecord {
+		response.Status(http.StatusNotFound).Error(consts.ErrChatNotFound).Send()
+		return
+	}
+
+	// 提取 history
+	histories, err := u.chatService.GetChatMessage(c, chatEntity)
+	var llmResonseChan = make(chan *llm.AssistantResponse)
+
+	// SSE
+	c.Writer.Header().Set("Content-Type", "text/event-stream")
+	c.Writer.Header().Set("Cache-Control", "no-cache")
+	c.Writer.Header().Set("Connection", "keep-alive")
+	c.Writer.Header().Set("Transfer-Encoding", "chunked")
+
+	go func() {
+		for {
+			select {
+			case msg := <-llmResonseChan:
+				//fmt.Println("接收到", msg.Content)
+				fmt.Print(msg.Content)
+
+				if msg.State == llm.StateDone {
+					fmt.Println("")
+					// 关闭 chan
+					close(llmResonseChan)
+
+					break
+				}
+			}
+		}
+	}()
+
+	err = u.llmService.StreamChat(llmResonseChan, histories)
+	if err != nil {
+		response.Status(http.StatusInternalServerError).Error(err).Send()
+	}
+
 }

e0b784b0021833585199a76c3e3e439712c9d9f0
Twilight <im@ivampiresp.com>
2024-08-01 18:03:31 +0800

    实现 function call

--- cmd/llm.go
+++ cmd/llm.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"log"
+
+	"github.com/bytedance/sonic"
 	"github.com/spf13/cobra"
 	"github.com/tmc/langchaingo/jsonschema"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/openai"
-	"log"
-	"rag-new/internal/base"
 )
 
 func init() {
@@ -26,72 +26,168 @@ var llmCmd = &cobra.Command{
 			return
 		}
 
-		//TestLLM(app)
-		TestCall(app)
+		llm, err := openai.New(
+			openai.WithToken(app.Config.OpenAI.ApiKey),
+			openai.WithBaseURL(app.Config.OpenAI.BaseUrl),
+			//openai.WithModel("qwen-turbo"),
+			openai.WithModel("qwen-turbo"),
+		)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		callLLM(llm)
+
+		//TestCall(app)
 
 	},
 }
 
-func TestLLM(app *base.Application) {
-	ctx := context.Background()
-
-	llm, err := openai.New(
-		openai.WithToken(app.Config.OpenAI.ApiKey),
-		openai.WithBaseURL(app.Config.OpenAI.BaseUrl),
-		openai.WithModel("qwen-max"),
-	)
-	if err != nil {
-		app.Logger.Sugar.Fatal(err)
-	}
-	prompt := "What would be a good company name for a company that makes colorful socks?"
-	completion, err := llms.GenerateFromSinglePrompt(ctx, llm, prompt)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(completion)
+type ChunkMessage struct {
+	Type     string        `json:"type"`
+	Function FunctionChunk `json:"function"`
 }
 
-func TestCall(app *base.Application) {
-	llm, err := openai.New(
-		openai.WithToken(app.Config.OpenAI.ApiKey),
-		openai.WithBaseURL(app.Config.OpenAI.BaseUrl),
-		openai.WithModel("qwen-max"),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx := context.Background()
-	resp, err := llm.GenerateContent(ctx,
-		[]llms.MessageContent{
-			llms.TextParts(llms.ChatMessageTypeHuman, "北京天气怎么样"),
-		},
-		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
-			fmt.Printf("Received chunk: %s\n", chunk)
-			return nil
-		}),
-		llms.WithTools(tools))
-	if err != nil {
-		log.Fatal(err)
-	}
+type FunctionChunk struct {
+	Name      string `json:"name"`
+	Arguments string `json:"arguments"`
+}
 
-	choice1 := resp.Choices[0]
-	if choice1.FuncCall != nil {
-		fmt.Printf("Function call: %v\n", choice1.FuncCall)
+type FunctionCallArgs map[string]interface{}
+
+func callLLM(llm *openai.LLM) {
+
+	var histories []llms.MessageContent
+	var requestAgain = true
+
+	for {
+		var question string
+		fmt.Print(">> ")
+		n, err := fmt.Scanln(&question)
+		if n == 0 || err != nil {
+			question = "北京天气怎么样"
+			fmt.Print(question + "\n")
+			//continue
+		}
+
+		requestAgain = true
+
+		histories = append(histories, llms.TextParts(llms.ChatMessageTypeHuman, question))
+
+		var fullResponse [][]byte
+
+		ctx := context.Background()
+
+		for {
+			fmt.Println("再次请求吗?" + fmt.Sprint(requestAgain))
+			if !requestAgain {
+				break
+			}
+
+			requestAgain = false
+			var isToolCall = false
+
+			fmt.Println("对话历史", histories)
+
+			resp, err := llm.GenerateContent(ctx,
+				histories,
+				llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+					// 检测长度
+					if len(chunk) == 0 {
+						return nil
+					}
+
+					fmt.Printf("Received chunk: %s\n", chunk)
+
+					fullResponse = append(fullResponse, chunk)
+
+					return nil
+				}),
+				llms.WithTools(tools))
+			if err != nil {
+				panic(err)
+			}
+
+			respchoice := resp.Choices[0]
+
+			if respchoice.FuncCall != nil {
+				fmt.Println("FunCall 检测到工具调用")
+				isToolCall = true
+				requestAgain = true
+			}
+
+			if isToolCall {
+				fmt.Printf("正在调用: %v\n", respchoice.FuncCall.Name)
+
+				var fullArgs = ""
+
+				assistantResponse := llms.TextParts(llms.ChatMessageTypeAI, respchoice.Content)
+				for _, tc := range respchoice.ToolCalls {
+					// 拼接参数
+					fullArgs += tc.FunctionCall.Arguments
+				}
+
+				var firstToolCall = respchoice.ToolCalls[0]
+				firstToolCall.FunctionCall.Arguments = fullArgs
+				assistantResponse.Parts = append(assistantResponse.Parts, firstToolCall)
+
+				histories = append(histories, assistantResponse)
+
+				fmt.Println("最终参数", fullArgs)
+
+				// 解析工具
+				var functionCallArgs FunctionCallArgs
+				err = sonic.Unmarshal([]byte(fullArgs), &functionCallArgs)
+				if err != nil {
+					panic(err)
+				}
+
+				switch respchoice.FuncCall.Name {
+				case "getCurrentWeather":
+					weatherInfo, err := getCurrentWeather(functionCallArgs["location"].(string), functionCallArgs["unit"].(string))
+					if err != nil {
+						panic(err)
+					}
+
+					histories = append(histories, llms.MessageContent{
+						Role: llms.ChatMessageTypeTool,
+						Parts: []llms.ContentPart{
+							llms.ToolCallResponse{
+								ToolCallID: respchoice.ToolCalls[0].ID,
+								Name:       respchoice.FuncCall.Name,
+								Content:    weatherInfo,
+							},
+						},
+					})
+
+				}
+
+			} else {
+				requestAgain = false
+			}
+
+			histories = append(histories, llms.TextParts(llms.ChatMessageTypeAI, resp.Choices[0].Content))
+
+			fmt.Println("本轮历史", histories)
+		}
+
 	}
 }
 
 func getCurrentWeather(location string, unit string) (string, error) {
-	weatherInfo := map[string]interface{}{
-		"location":    location,
-		"temperature": "72",
-		"unit":        unit,
-		"forecast":    []string{"sunny", "windy"},
-	}
-	b, err := json.Marshal(weatherInfo)
-	if err != nil {
-		return "", err
-	}
-	return string(b), nil
+	//weatherInfo := map[string]interface{}{
+	//	"location":    location,
+	//	"temperature": "72",
+	//	"unit":        unit,
+	//	"forecast":    []string{"sunny", "windy"},
+	//}
+	//b, err := json.Marshal(weatherInfo)
+	//if err != nil {
+	//	return "", err
+	//}
+	//return string(b), nil
+
+	return "晴天", nil
 }
 
 // json.RawMessage(`{"type": "object", "properties": {"location": {"type": "string", "description": "The city and state, e.g. San Francisco, CA"}, "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]}}, "required": ["location"]}`),

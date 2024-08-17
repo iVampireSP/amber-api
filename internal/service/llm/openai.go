680138f78a7ca15517d3a85d63a880786d4b3c59
ivamp <im@ivampiresp.com>
2024-08-17 20:04:08 +0800

    改进 警告限制

--- internal/service/llm/openai.go
+++ internal/service/llm/openai.go
@@ -18,8 +18,8 @@ import (
 // 强制停止（如果连续函数调用超过 4 次，则强制停止输出）
 const forceStopCount = 4
 
-// 警告次数（如果 LLM 连续调用超过 2次，则警告输出）
-const warningCount = 2
+// 警告次数（如果 LLM 连续调用超过 3 次，则警告输出）
+const warningCount = 3
 
 // 警告 LLM 调用太多次工具， 要求停止
 const warningMessage = "[Warning]You are attempting to call the tool/function repeatedly, please use the tool/function properly. If you continue to call repeatedly, the chat will be forcibly terminated."

ebecbb417dd0bbcb0c97cea6ee481cd1064532e2
Twilight <im@ivampiresp.com>
2024-08-31 02:53:34 +0800

    增加 外键

---
+++ internal/migrations/2_add_foreign_key.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+-- 新建外键
+ALTER TABLE assistant_tools ADD CONSTRAINT assistant_tools_assistant_id_foreign FOREIGN KEY (assistant_id) REFERENCES assistants (id);
+ALTER TABLE assistant_tools ADD CONSTRAINT assistant_tools_tool_id_foreign FOREIGN KEY (tool_id) REFERENCES tools (id);
+
+-- +goose Down
+-- 删除外键
+ALTER TABLE assistant_tools DROP FOREIGN KEY assistant_tools_assistant_id_foreign;
+ALTER TABLE assistant_tools DROP FOREIGN KEY assistant_tools_tool_id_foreign;

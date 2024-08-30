401d6e78b4d2f33aa9ba21e941f856b9c591f856
Twilight <im@ivampiresp.com>
2024-08-31 01:03:45 +0800

    修复 json 类型问题

--- internal/entity/Model.go
+++ internal/entity/Model.go
@@ -1,21 +1,20 @@
 package entity
 
 import (
-	"gorm.io/gorm"
 	"rag-new/internal/schema"
 	"time"
 )
 
-type Base struct {
-	ID        int64          `gorm:"primarykey" json:"id"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index"`
-}
+//type Base struct {
+//	ID        int64          `gorm:"primarykey" json:"id"`
+//	CreatedAt time.Time      `json:"created_at"`
+//	UpdatedAt time.Time      `json:"updated_at"`
+//	DeletedAt gorm.DeletedAt `gorm:"index"`
+//}
 
 // Model 是所有 entity 的基类，后期要将所有的 Base 改成这种形式
 type Model struct {
-	Id        schema.EntityId `gorm:"primarykey"  json:"string,id"`
+	Id        schema.EntityId `gorm:"primarykey" json:"id,string"`
 	CreatedAt time.Time       `json:"created_at"`
 	UpdatedAt time.Time       `json:"updated_at"`
 	//DeletedAt gorm.DeletedAt  `gorm:"index"`

ce66bb014c55110bbe532d858cdcf6c219145b04
Twilight <im@ivampiresp.com>
2024-09-03 14:10:42 +0800

    移除 XORM Tag

--- internal/entity/File.go
+++ internal/entity/File.go
@@ -7,11 +7,11 @@ import (
 type File struct {
 	Model
 
-	Url      *string `xorm:"varchar(255) notnull" json:"url"`
-	UrlHash  *string `xorm:"varchar(255) notnull" json:"url_hash"`
-	FileHash string  `xorm:"varchar(255) notnull" json:"file_hash"`
-	MimeType string  `xorm:"mime_type int(11) notnull" json:"mime_type"`
-	Path     string  `xorm:"varchar(255) notnull" json:"path"`
+	Url      *string `json:"url"`
+	UrlHash  *string `json:"url_hash"`
+	FileHash string  `json:"file_hash"`
+	MimeType string  `json:"mime_type"`
+	Path     string  `json:"path"`
 	// TODO: 移除 file 的到期时间，如果当 file 没有任何引用的时候再删除
 	// 因为有外键，所以直接删除是删不掉的，必须删除消息
 	ExpiredAt *time.Time `xorm:"TIMESTAMP null" json:"expired_at"`

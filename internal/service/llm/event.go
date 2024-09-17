cf059bb83c1c0c4b50a180d004284329c6d3e8f8
Twilight <im@ivampiresp.com>
2024-09-17 14:49:23 +0800

    停止 chunk 事件发送

--- internal/service/llm/event.go
+++ internal/service/llm/event.go
@@ -2,20 +2,21 @@ package llm
 
 import (
 	"context"
-	"rag-new/internal/message"
 	"rag-new/internal/schema"
 )
 
-func (s *Service) event(ctx context.Context, user *schema.UserPublicInfo, msg *schema.AssistantResponse) {
+func (s *Service) event(_ context.Context, _ *schema.UserPublicInfo, msg *schema.AssistantResponse) {
 	switch msg.State {
 	case schema.StateChunk:
 		// 这个会顺序错误
 		// 但是目前没关系，因为还没到真正的用途
-		chunkMessage := s.message.NewChunk(msg.Content, user)
-		err := s.streamService.SendEvent(ctx, message.Chunk.String(), chunkMessage)
-		if err != nil {
-			s.Logger.Sugar.Errorf("Unable send event: %v", err)
-		}
+		//chunkMessage := s.message.NewChunk(msg.Content, user)
+		//err := s.streamService.SendEvent(ctx, message.Chunk.String(), chunkMessage)
+		//if err != nil {
+		//	s.Logger.Sugar.Errorf("Unable send event: %v", err)
+		//}
+
+		// 算了，目前没必要
 
 		return
 		//case schema.StateToolSuccess:

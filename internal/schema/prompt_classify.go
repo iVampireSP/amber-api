4a3759e6367356598847957ca76444e3376fc9a6
ivamp <im@ivampiresp.com>
2024-10-24 14:25:18 +0800

    改进 数学计算部分

--- internal/schema/prompt_classify.go
+++ internal/schema/prompt_classify.go
@@ -5,14 +5,16 @@ type QuestionLabel string
 
 var (
 	QuestionLabelSimpleTask       QuestionLabel = "Simple tasks"
-	QuestionLabelComplexReasoning QuestionLabel = "complex reasoning"
-	QuestionLabelCreativeWriting  QuestionLabel = "creative writing"
+	QuestionLabelComplexReasoning QuestionLabel = "Complex reasoning"
+	QuestionLabelCreativeWriting  QuestionLabel = "Creative writing"
+	QuestionLabelMath             QuestionLabel = "Math"
 )
 
 var QuestionLabels = []string{
 	string(QuestionLabelCreativeWriting),
 	string(QuestionLabelComplexReasoning),
 	string(QuestionLabelSimpleTask),
+	string(QuestionLabelMath),
 }
 
 var (
@@ -25,6 +27,18 @@ var (
 观察：行动的结果
 思考：我现在知道最终答案了
 最终答案：原始输入问题的最终答案
+`
+	PromptClassifyMath PromptClassify = `
+当你在进行数学计算时，使用以下输出
+问题: 你必须要回答的问题
+思考：你应该始终思考该做什么
+操作：要采取的操作，你要是用什么工具，或者思考逻辑
+动作输入：动作的输入
+观察：行动的结果
+思考：我现在知道最终答案了
+最终答案：原始输入问题的最终答案
+
+计算时，你必须使用计算器工具，无论如何都不允许使用自己的知识或不计算进行输出，计算器永远比你正确的并且不会出错。
 `
 )
 
@@ -39,16 +53,16 @@ func (ql QuestionLabel) IsValid() bool {
 }
 
 func (ql QuestionLabel) Prompt() string {
-	if ql == QuestionLabelSimpleTask {
-		return ""
+	var prompt = ""
+
+	switch ql {
+	case QuestionLabelComplexReasoning:
+		prompt = PromptClassifyReAct.String()
+	case QuestionLabelMath:
+		prompt = PromptClassifyMath.String()
 	}
 
-	if ql == QuestionLabelComplexReasoning {
-		//return "请用更复杂的逻辑回答问题，不要直接输出答案，而是给出推理过程，并给出推理的步骤，最后给出答案。"
-		return PromptClassifyReAct.String()
-	}
-
-	return ""
+	return prompt
 }
 
 func (pc PromptClassify) String() string {
